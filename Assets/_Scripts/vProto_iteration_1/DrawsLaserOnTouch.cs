c3ee773aa37113224e9f9161bf073e4bc7181935
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-02-11 17:33:04 -0300

    rebotes de burbujas

--- Assets/_Scripts/vProto_iteration_1/DrawsLaserOnTouch.cs
+++ Assets/_Scripts/vProto_iteration_1/DrawsLaserOnTouch.cs
@@ -4,81 +4,123 @@ using UnityEngine;
 
 public class DrawsLaserOnTouch : MonoBehaviour
 {
-    public int vertexIdx = 1;
-    bool iactive = true;
-    Vector3 currentRot;
-    Vector3 currentPos;
-
     public LineRenderer lineRend;
-    public float maxDist = 10;//max distance for beam to travel.
     public string bounceTag = "bouncer";//tag it can reflect off.
-    public int maxReflections = 3;
+    public float maxDist = 100;//max distance for beam to travel.
+    public int maxSplitCount = 5;
 
-    // Start is called before the first frame update
-    void Start()
+    Vector2 lastMousePos;
+
+    private void Start()
     {
+        lineRend = GetComponent<LineRenderer>();
+        lineRend.positionCount = 2;
+        lineRend.SetPosition(0, transform.position);
 
     }
+
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            DrawLaser();
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+
+            var dirMouse = (transform.position - mousePos).normalized * maxDist;
+
+            if (lineRend.positionCount < 2)
+                lineRend.positionCount = 2;
+
+            lineRend.SetPosition(0, transform.position);
+            lineRend.SetPosition(1, mousePos);
+
+
+            DrawLaserReflection(this.transform.position, -dirMouse, maxSplitCount);
+
         }
         else
         {
             lineRend.positionCount = 1;
-            vertexIdx = 1;
-
 
         }
     }
 
-    void DrawLaser()
+    void DrawLaserReflection(Vector2 position, Vector2 direction, int reflectRemaining)
     {
-        vertexIdx = 2;
-        iactive = true;
-        currentRot = transform.up;
-        currentPos = transform.localPosition;
-        lineRend.positionCount = 2;
-        lineRend.SetPosition(0, transform.localPosition);
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0;
+        RaycastHit2D hit2D = Physics2D.Raycast(position, direction, maxDist);
 
-        lineRend.SetPosition(1, mousePos - transform.position);
-
-        while (iactive)
+        if (hit2D)
         {
-            vertexIdx++;
-            RaycastHit rHit = new RaycastHit();
-            lineRend.positionCount = vertexIdx;
-            if (Physics.Raycast(currentPos, currentRot, out rHit)) //rHit, maxDist))
+            Debug.Log("hitting something");
+            lineRend.positionCount = maxSplitCount - reflectRemaining + 2; //+ 3
+            Debug.Log(maxSplitCount - reflectRemaining + 2 + " ,, ref rem " + reflectRemaining ); //+ 1
+            lineRend.SetPosition(reflectRemaining, hit2D.point); //+ 1
+
+            if (hit2D.transform.gameObject.CompareTag(bounceTag))
             {
-                //verti++;
-                currentPos = rHit.point;
-                currentRot = Vector3.Reflect(currentRot, rHit.normal);
-                lineRend.SetPosition(vertexIdx - 1, rHit.point);
-                if (rHit.transform.gameObject.tag != bounceTag)
+                Debug.Log("bouncing at " + hit2D.point);
+                direction = Vector2.Reflect(direction, hit2D.normal);
+                position = hit2D.point + direction * 0.01f;
+
+                if (reflectRemaining > 0)
                 {
-                    iactive = false;
+                    DrawLaserReflection(position, direction, --reflectRemaining);
                 }
             }
-            else
-            {
-                //verti++;
-                iactive = false;
-                lineRend.SetPosition(vertexIdx - 1, currentPos + 100 * currentRot);
-
-            }
-            if (vertexIdx > maxReflections)
-            {
-                iactive = false;
-            }
-
-
         }
     }
+
+    //void DrawLaser()
+    //{
+    //    vertexIdx = 2;
+    //    iactive = true;
+    //    currentRot = transform.up;
+    //    //currentPos = transform.localPosition;
+    //    currentPos = transform.position;
+
+    //    lineRend.positionCount = 2;
+    //    //lineRend.SetPosition(0, transform.localPosition);
+    //    lineRend.SetPosition(0, transform.position);
+    //    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //    mousePos.z = 0;
+
+    //    lineRend.SetPosition(1, mousePos - transform.position);
+
+    //    while (iactive)
+    //    {
+    //        vertexIdx++;
+    //        RaycastHit rHit = new RaycastHit();
+    //        lineRend.positionCount = vertexIdx;
+    //        Debug.DrawRay(currentPos, (lineRend.GetPosition(1) - currentPos).normalized * maxDist, Color.green);
+    //        Debug.Log(Physics.Raycast(currentPos, (lineRend.GetPosition(1) - currentPos).normalized * maxDist, out rHit));
+    //        if (Physics.Raycast(currentPos, (lineRend.GetPosition(1) - currentPos).normalized * maxDist, out rHit)) //rHit, maxDist))
+    //        {
+    //            Debug.Log(rHit.collider.name);
+    //            //verti++;
+    //            currentPos = rHit.point;
+    //            currentRot = Vector3.Reflect(currentRot, rHit.normal);
+    //            lineRend.SetPosition(vertexIdx - 1, rHit.point);
+    //            if (rHit.transform.gameObject.tag != bounceTag)
+    //            {
+    //                iactive = false;
+    //            }
+    //        }
+    //        else
+    //        {
+    //            //verti++;
+    //            iactive = false;
+    //            lineRend.SetPosition(vertexIdx - 1, currentPos + 100 * currentRot);
+
+    //        }
+    //        if (vertexIdx > maxReflections)
+    //        {
+    //            iactive = false;
+    //        }
+
+
+    //    }
+    //}
 }
 
---
+++ Assets/_Scripts/vProto_iteration_1/Laser.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public int maxReflectionCount = 5;
+    public int maxSplitCount = 5;
+    public float maxStepDistance = 100;
+
+    private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        var ghjkagsdkhm = (transform.position - mousePos).normalized * maxStepDistance;
+        DrawPredictedReflection(this.transform.position, -ghjkagsdkhm, maxReflectionCount, maxSplitCount);
+    }
+
+    void DrawPredictedReflection(Vector2 position, Vector2 direction, int reflectionsRemaining, int splitsRemaining)
+    {
+        var gizmoHue = (reflectionsRemaining / (this.maxReflectionCount + 1f));
+        Gizmos.color = Color.HSVToRGB(gizmoHue, 1, 1);
+        RaycastHit2D hit2D = Physics2D.Raycast(position, direction, maxStepDistance);
+
+        if (hit2D) //did we hit somthing?
+        {
+            Gizmos.DrawLine(position, hit2D.point);
+            Gizmos.DrawWireSphere(hit2D.point, 0.25f);
+
+            if (hit2D.transform.gameObject.tag == "Receiver")
+            {
+                Debug.Log("Receiver hit");
+            }
+            if (hit2D.transform.gameObject.tag == "bouncer") //mirror hit. set new pos where hit. reflect angle and make that new direction
+            {
+                //Debug.Log("Mirror Hit");
+                direction = Vector2.Reflect(direction, hit2D.normal);
+                position = hit2D.point + direction * 0.01f;
+
+                if (reflectionsRemaining > 0)
+                    DrawPredictedReflection(position, direction, --reflectionsRemaining, splitsRemaining);
+            }
+            if (hit2D.transform.gameObject.tag == "Splitter") //reflect and go ahead
+            {
+
+                Debug.Log("Splitter hit");
+                if (splitsRemaining > 0)//go ahead
+                {
+                    Debug.Log("Splitting");
+                    Vector2 splitPosition = new Vector2();
+                    Vector2 findOppBegin = hit2D.point + direction * 1f;
+                    RaycastHit2D[] findOppHit = Physics2D.RaycastAll(findOppBegin, -direction);
+                    for (int i = 0; i <= findOppHit.Length; i++) //findOppHit[i].transform.gameObject != hit2D.transform.gameObject
+                    {
+                        if (findOppHit[i].transform.gameObject == hit2D.transform.gameObject)
+                        {
+                            splitPosition = findOppHit[i].point + direction * 0.01f;
+                            break;
+                        }
+                    }
+
+                    DrawPredictedReflection(splitPosition, direction, reflectionsRemaining, --splitsRemaining);
+                }
+
+
+                direction = Vector2.Reflect(direction, hit2D.normal);
+                position = hit2D.point + direction * 0.01f;
+                if (reflectionsRemaining > 0)//reflect too
+                {
+                    DrawPredictedReflection(position, direction, --reflectionsRemaining, splitsRemaining);
+                }
+            }
+        }
+    }
+}
