bbc75385a2fff6ea949dedf1a074713e883cf463
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-02-22 16:05:49 -0300

    shooter fix priority

--- Assets/_Scripts/vProto_iteration_1/BubbleShooter.cs
+++ Assets/_Scripts/vProto_iteration_1/BubbleShooter.cs
@@ -22,66 +22,52 @@ public class BubbleShooter : MonoBehaviour
     {
         //for (int i = 0; i < throwableCount - currentThrowables.Count; i++) //0 - 2
         int i = 0;
-        while (throwableCount != currentThrowables.Count)
+        while (throwableCount > currentThrowables.Count)
         {
             Bubble bubble = Instantiate(ObjectPooler.instance.pools[0].prefab).GetComponent<Bubble>();
             bubble.GenerateThrowableBubble();
             currentThrowables.Enqueue(bubble);
 
-            bubble.transform.parent = i == 0 ? spawnPrimaryBubble.transform : spawnSecondaryBubble.transform;
-            bubble.transform.position = i == 0 ? spawnPrimaryBubble.transform.position : spawnSecondaryBubble.transform.position;
+            bubble.transform.parent = spawnPrimaryBubble.transform.childCount == 0 ? spawnPrimaryBubble.transform : spawnSecondaryBubble.transform;
+            //bubble.transform.position = i == 0 ? spawnPrimaryBubble.transform.position : spawnSecondaryBubble.transform.position;
+            bubble.transform.localPosition = Vector3.zero;
+
             i++;
         }
     }
 
     /// <summary>
-    /// ACA TENGO QUE HACER QUE LA BURBUJA QUE SE GENERA SE QUEDE EN EL LUGAR
-    /// PORQUE ME ESTA CAGANDO LA MECANICA DEL LASER
+    // Switch current bubble 2 -> 1 
     /// </summary>
     void SwitchBubblePriority()
     {
+
+        var auxBubbleSec = currentThrowables.Dequeue(); // first in
+
+        auxBubbleSec.transform.parent = spawnPrimaryBubble.transform;
+        auxBubbleSec.transform.localPosition = Vector3.zero;
+
+        currentThrowables.Enqueue(auxBubbleSec);
+
+        if (currentThrowables.Count <= 1)
+            FeedBubbleShooter();
         
-
-
-        var auxQ = currentThrowables.Dequeue(); // first in
-        var auxQ2 = currentThrowables.Dequeue(); // second in
-
-        currentThrowables.Enqueue(auxQ2);
-        currentThrowables.Enqueue(auxQ);
-
-
-        auxQ2.transform.parent = spawnSecondaryBubble.transform;
-        auxQ2.transform.position = spawnSecondaryBubble.transform.position;
-
-
-
-        //auxQ.transform.parent = spawnPrimaryBubble.transform;
-        //auxQ.transform.position = spawnPrimaryBubble.transform.position;
-
-        var aux = spawnPrimaryBubble;
-        spawnPrimaryBubble = spawnSecondaryBubble;
-        spawnSecondaryBubble = aux;
-
-        //var auxpos = spawnPrimaryBubble.transform.position;
-        //spawnPrimaryBubble.transform.position = spawnSecondaryBubble.transform.position;
-        //spawnSecondaryBubble.transform.position = auxpos;
     }
 
 
     public void OnBubbleThrow()
     {
         var thrown = currentThrowables.Dequeue();
-
         thrown.transform.parent = null;
+
 
         var force = (Utils.instance.MouseToWorldWithoutZ() - transform.position).normalized;
         force *= forceMagnitudeShoot;
 
         thrown.SetImpulseForce(force);
 
-        FeedBubbleShooter();
-        CurrentBubbleSwitch.instance.onSwitchBubble.Invoke();
 
+        CurrentBubbleSwitch.instance.onSwitchBubble.Invoke();
     }
 
 
