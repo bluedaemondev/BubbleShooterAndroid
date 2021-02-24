b86dcdb702b0902f98a92675204272f38668a19f
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-02-24 17:19:03 -0300

    mejora general, carga de musica en inicio, componentes para ui, testing, fuente, loop tests

---
+++ Assets/_Scripts/vProto_iteration_1/Mecanicas/LoadMusicOnStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadMusicOnStart : MonoBehaviour
+{
+    public AudioClip musicOnStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundManager.instance.PlayMusic(musicOnStart);
+    }
+}
---
+++ Assets/_Scripts/vProto_iteration_1/UI/SoundOnClickUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Button = UnityEngine.UI.Button;
+[RequireComponent(typeof(Button))]
+public class SoundOnClickUI : MonoBehaviour
+{
+    public AudioClip clipToPlay;
+    Button btnComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnComponent = GetComponent<Button>();
+        btnComponent.onClick.AddListener(PlayClip);
+    }
+    void PlayClip()
+    {
+        SoundManager.instance.PlayEffect(clipToPlay);
+    }
+}
