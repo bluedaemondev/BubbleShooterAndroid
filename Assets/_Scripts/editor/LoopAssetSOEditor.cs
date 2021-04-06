26c829583de15607c71a8a9b330b6a23de1bdc6f
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-04-06 16:47:16 -0300

    asset editor

---
+++ Assets/_Scripts/editor/LoopAssetSOEditor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BackgroundAndMusic))]
+public class LoopAssetSOEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        BackgroundAndMusic backgroundAndMusicSO = (BackgroundAndMusic)target;
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        GUILayout.Label("OUTPUT", new GUIStyle { fontStyle = FontStyle.Bold });
+        GUILayout.FlexibleSpace();
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+
+        EditorGUILayout.BeginVertical();
+
+        Texture texture = null;
+        if (backgroundAndMusicSO.SpriteBackground != null)
+        {
+            texture = backgroundAndMusicSO.SpriteBackground.texture;
+        }
+        GUILayout.Box(texture, GUILayout.Width(150), GUILayout.Height(150));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("SpriteBackground"), GUIContent.none, true, GUILayout.Width(150));
+        EditorGUILayout.EndVertical();
+
+        GUILayout.FlexibleSpace();
+        EditorGUILayout.EndHorizontal();
+
+
+
+        EditorGUILayout.Space();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
